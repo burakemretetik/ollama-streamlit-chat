8434c821eefc6314e268f40aa0fb762da2b803ab
Burak Tetik <burakemretetik@gmail.com>
2024-12-16 17:15:06 +0300

    Update ollama-streamlit-app.py

--- ollama-streamlit-app.py
+++ ollama-streamlit-app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from llama_index.core.llms import ChatMessage
 import logging
 import time
-from llama_index import Ollama
+from ollama import Ollama 
 
 logging.basicConfig(level=logging.INFO)
 
